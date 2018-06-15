336c94b1b89dae10eeaf7ac9564f7226bc01fe8e
Conor Turner <conorturner@live.com>
2018-06-15 21:03:11 +0100

    improve json body parser

--- library/JsonBodyParser.js
+++ library/JsonBodyParser.js
@@ -2,20 +2,29 @@ class JsonBodyParser {
 
 	static middleware() {
 		return (req, res, next) => {
+			const tryParse = (body) => {
+				try {
+					return JSON.parse(body);
+				}
+				catch (e) {
+				}
+			};
 
-			let body = [];
+			if (req.headers["content-type"] !== "application/json") return next();
 
-			req
-				.on("data", (chunk) => body.push(chunk))
-				.on("end", () => {
-					try {
-						req.body = JSON.parse(Buffer.concat(body).toString());
-					}
-					catch (e) {
-					}
-
-					next();
-				});
+			if (req.body) {
+				req.body = tryParse(req.body);
+				next();
+			}
+			else {
+				let body = [];
+				req
+					.on("data", (chunk) => body.push(chunk))
+					.on("end", () => {
+						req.body = tryParse(Buffer.concat(body));
+						next();
+					});
+			}
 
 		};
 	}
